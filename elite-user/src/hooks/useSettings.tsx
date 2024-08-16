73f10b488bc369579626f606b47771e15d77bc2e
himanshu-kumar-kune <himanshu.kumar@kune.consulting>
2024-08-16 16:13:12 +0530

    FIRST COMMIT

--- elite-user/src/hooks/useSettings.tsx
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react';
-import { settingsContext } from '../contexts/settingsContext';
-
-
-const useSettings = () => useContext(settingsContext);
-
-export default useSettings;
